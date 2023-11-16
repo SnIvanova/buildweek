e076205a6beee4ef1566a00d9814586d30bde94d
SnIvanova <1snizhanaivanova@gmail.com>
2023-11-16 12:04:28 +0100

    chart

--- js/ResultsPage.js
+++ js/ResultsPage.js
@@ -1,24 +1,26 @@
-document.addEventListener('DOMContentLoaded', function (){
+document.addEventListener('DOMContentLoaded', function () {
+    const match = 8;
+    const unmatch = 2; 
 
-})
+  
+    const answer = {
+        match,
+        unmatch,
+    };
 
-const answer = {
-    match,
-    wrong,
-};
-
-const total = answer.match + answer.wrong;
+    const total = answer.match + answer.unmatch;
     const correctPercentage = ((answer.match / total) * 100).toFixed(1);
-    const wrongPercentage = ((answer.wrong / total) * 100).toFixed(1);
+    const wrongPercentage = ((answer.unmatch / total) * 100).toFixed(1);
 
-const results = document.getElementsByClassName('resultsItem');
+
+    const results = document.getElementsByClassName('resultsItem');
     createResultElement('Correct', correctPercentage, answer.match, results[0], 'left');
-    createResultElement('Wrong', wrongPercentage, answer.wrong, results[2], 'right');
+    createResultElement('Wrong', wrongPercentage, answer.unmatch, results[2], 'right');
 
-const canvas = document.getElementById('myCanvas');
-    DoughnutChart(canvas, wrongPercentage, correctPercentage);
+    const canvas = document.getElementById('myCanvas');
+    configureDoughnutChart(canvas, wrongPercentage, correctPercentage);
 
-function createParagraph(text, fontSize, margin, fontWeight) {
+    function createParagraph(text, fontSize, margin, fontWeight) {
         const paragraph = document.createElement('p');
         paragraph.innerText = text;
         paragraph.style.fontSize = fontSize;
@@ -26,8 +28,8 @@ function createParagraph(text, fontSize, margin, fontWeight) {
         paragraph.style.fontWeight = fontWeight;
         return paragraph;
     }
- 
-function createResultElement(title, percentage, count, container, textAlign) {
+
+    function createResultElement(title, percentage, count, container, textAlign) {
         const titleElement = createParagraph(title, '3em', '0', '1em');
         const percentageElement = createParagraph(percentage + '%', '3em', '0', 'bold');
         const countElement = createParagraph(`${count}/${total} questions`, '0', '0', '100px');
@@ -38,4 +40,77 @@ function createResultElement(title, percentage, count, container, textAlign) {
         container.appendChild(countElement);
         container.appendChild(emptyElement);
         container.style.textAlign = textAlign;
-    }    
+    }
+
+    function configureDoughnutChart(canvas, wrongPercentage, correctPercentage) {
+        const context = canvas.getContext('2d');
+        const backgroundColors = ['#D20094', '#00FFFF'];
+        const cutoutPercentage = '70%';
+
+        if (wrongPercentage < 50) {
+            document.getElementById('right').style.opacity = '0.8';
+        } else {
+            document.getElementById('left').style.opacity = '0.8';
+            configureSorryText(context, canvas.width, canvas.height);
+        }
+
+        new Chart(context, {
+            type: 'doughnut',
+            data: {
+                datasets: [{
+                    label: 'Result',
+                    data: [wrongPercentage, correctPercentage],
+                    backgroundColor: backgroundColors,
+                    cutout: cutoutPercentage,
+                    hoverOffset: 1,
+                    borderColor: 'transparent',
+                }]
+            },
+            options: {
+                borderAlign: 'inner',
+            },
+            plugins: [{
+                id: 'text',
+                beforeDatasetsDraw: function (chart) {
+                    if (wrongPercentage < 50) {
+                        configureCongratulationsText(context, canvas.width, canvas.height);
+                        configureCertificateText(context, canvas.width, canvas.height);
+                    } else{configureSorryText(context, canvas.width, canvas.height);
+                    }
+                },
+                   
+            }],
+        });
+    }
+
+    function configureCongratulationsText(ctx, width, height) {
+        configureText(ctx, 'Congratulations!', 'white', width , height, 2.9);
+        configureText(ctx, 'You passed the exam', 'aqua', width, height, 2.4);
+    }
+
+    function configureCertificateText(ctx, width, height) {
+        configureText(ctx, `We'll send you the certificate in`, 'white', width, height, 1.8);
+        configureText(ctx, 'in few minutes.', 'white', width, height, 1.65);
+        configureText(ctx, 'Check your email (including', 'white', width, height, 1.55);
+        configureText(ctx, 'promotions/spam folder)', 'white', width, height, 1.45);
+    }
+
+    function configureSorryText(ctx, width, height) {
+        configureText(ctx, 'Sorry!', 'white', width, height, 2.5);
+        configureText(ctx, "You didn't pass the exam", 'red', width, height, 2);
+    }
+
+    function configureText(ctx, text, color, width, height, ratio) {
+        ctx.restore();
+        const fontSize = (height / 350);
+        ctx.font = `${fontSize}em sans-serif`;
+        ctx.textBaseline = 'middle';
+        ctx.textAlign = 'center'; 
+    
+        const textX = width /  ratio; 
+        const textY = height / ratio;
+    
+        ctx.fillStyle = color;
+        ctx.fillText(text, textX, textY);
+    }
+});
